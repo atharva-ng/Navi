0c88aa68c3d87fa694e6a995a3478057a1b5c403
Atharva Ghadi <atharva_main@Atharvas-MacBook-Pro.local>
2022-09-12 18:58:18 +0530

    New workday push

---
+++ lib/bottomSheet.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class bottomSheet extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return bottomSheet();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,12 +20,34 @@ class MyApp extends StatelessWidget {
 
 class homeScreen extends StatelessWidget {
 
+  void sheet(BuildContext ctx)
+  {
+    showModalBottomSheet(
+     context: ctx,
+     isScrollControlled: true,
+     builder: (_){
+      return Container(child: Column(
+        children: const [
+          SizedBox(height: 50,),
+          Text("data"),
+        ],
+      ));
+     }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("TEST"),),
-
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 10,width: 20,),
+            Container(child: RaisedButton(onPressed: () => sheet(context) )),
+          ],
+        ),
+      ),
     );
-    
   }
 }
